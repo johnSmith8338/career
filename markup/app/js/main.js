614474b35106b59dc2f5b34ba4e29d1b8e311a74
KomP\USER <zaporozhan83@gmail.com>
2018-02-22 20:54:09 +0200

    Modal window was added

--- markup/app/js/main.js
+++ markup/app/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     anchorLinks();
     mobileMenu();
+    modalWindow();
     initMap();
 });
 
@@ -21,6 +22,35 @@ function anchorLinks() {
 function mobileMenu() {
     $(".hidden-menu").click(function () {
         $(".nav-list").toggleClass('nav-list-active');
+    });
+}
+
+// modal
+function modalWindow() {
+    $('button#go').click(function (event) {
+        event.preventDefault();
+        $('#overlay').fadeIn(400,
+            function () {
+                $('#modal-form')
+                    .css('display', 'block')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    });
+
+    $('#modal-close, #overlay').click(function () {
+        $('#modal-form')
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    $('#overlay').fadeOut(400);
+                }
+            );
     });
 }
 
